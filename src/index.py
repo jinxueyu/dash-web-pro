88d632487b3db26172c9e78c917b6eeb4be75427
jinxueyu <287665530@qq.com>
2021-09-10 12:57:33 +0800

    init

--- src/index.py
+++ src/index.py
@@ -1,15 +1,11 @@
-from dash import Output, Input, html, dcc, State, ALL
+from dash import html, dcc
 
 from core.app import app
-from controller.nlp import NlpController
 
 from callbacks import menu
 from callbacks import chat
 from callbacks import nlp
 from core import callbacks
-
-from core import controller
-from layouts import login, chat
 
 
 app.layout = html.Div([
